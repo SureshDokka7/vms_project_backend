be9c067c1c61a3f6b166c95407a585ed0104f531
Suresh Dokka <112100121+Suresh-d-7@users.noreply.github.com>
2025-02-20 00:08:18 +0530

    Initial Setup for VMS Backend.

---
+++ app/controllers/volunteerRegistrationController.js
@@ -0,0 +1,14 @@
+const asyncHandler = require("express-async-handler");
+const { registerUser, loginUser } = require("../services/userService");
+
+exports.register = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await registerUser(name, email, password);
+  res.status(201).json(user);
+});
+
+exports.login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const result = await loginUser(email, password);
+  res.json(result);
+});
---
+++ app/services/volunteerRegistrationService.js
@@ -0,0 +1,27 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (name, email, password) => {
+  const userExists = await User.findOne({ email });
+  if (userExists) throw new Error("User already exists");
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  return { id: user._id, name: user.name, email: user.email };
+};
+
+const loginUser = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("Invalid credentials");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error("Invalid credentials");
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+  return { token, user: { id: user._id, name: user.name, email: user.email } };
+};
+
+module.exports = { registerUser, loginUser };
