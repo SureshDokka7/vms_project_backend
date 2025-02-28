376522dc7c3d2589a86b8f7f14b0ae176a53e650
Suresh Dokka <112100121+Suresh-d-7@users.noreply.github.com>
2025-02-28 23:35:25 +0530

    Suresh - Default Server Page Message added from the Backend.

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api/users", require("./app/routes/volunteerRegistrationRoutes"));
+app.use("/api/v1", require("./app/routes/volunteerRegistrationRoutes"));
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => res.send("Welcome to Volunteer Registration API"));
+app.get("*", (req, res) => res.send("404 Page Not Found"));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
